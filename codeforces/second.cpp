3a33ebb0f6e6b6a973f8579fb81c8c2076d953d4
hasan <hasanrashed537@gmail.com>
2020-08-08 22:06:15 +0600

    second program

---
+++ codeforces/second.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(void)
+{
+    int a, b, c;
+
+    cout << "Enter the value of a and b: ";
+    cin >> a >> b;
+
+    c = a + b;
+    cout << c;
+
+
+    return 0;
+}
